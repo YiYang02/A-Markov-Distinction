55cf09731eea43db42abdc3166bd0ff6ee7999b7
Yi Yang <yyang2@bowdoin.edu>
2023-09-15 02:07:29 -0500

    add more comments to code

--- markov_code.py
+++ markov_code.py
@@ -1,14 +1,27 @@
+"""
+Markov Kaleidoscope
+Mission 3: A Markov Distinction
+Yi Yang
+14 Sept, 2023
+
+Class to generate a kaleidoscope pattern using Markov Chains.
+"""
+
 import math
 import random
 from PIL import Image, ImageDraw
 
 class Kaleidoscope:
-    """Class to create and manage the kaleidoscope image."""
-
+    """
+    Initializes the kaleidoscope settings and properties.
+    """
     def __init__(self, width, height):
+        
         self.WIDTH = width
         self.HEIGHT = height
         self.BACKGROUND_COLOR = (255, 255, 255)
+
+        # Color definitions and transitions
         self.COLORS = {
             "Violet": (148, 0, 211),
             "Indigo": (75, 0, 130),
@@ -33,13 +46,16 @@ class Kaleidoscope:
             "Pink": {"Red": 0.2, "Pink": 0.4, "Violet": 0.3, "Indigo": 0.1},
         }
 
+        # Shape definitions and transitions
+        self.SHAPE_ARC = 'arc'
+        self.SHAPE_ELLIPSE = 'ellipse'
         self.SHAPE_TRANSITIONS = {
             'arc': {'arc': 0.5, 'ellipse': 0.5},
             'ellipse': {'arc': 0.6, 'ellipse': 0.4}
         }
 
+        # Sides definitions and transitions
         self.SIDES = [5, 10, 15, 20, 25, 30]
-
         self.SIDES_TRANSITIONS = {
             5: {5: 0.1, 10: 0.3, 15: 0.3, 20: 0.2, 25: 0.1},
             10: {5: 0.1, 10: 0.3, 15: 0.3, 20: 0.2, 25: 0.1},
@@ -48,19 +64,24 @@ class Kaleidoscope:
             25: {15: 0.1, 20: 0.3, 25: 0.3, 30: 0.3},
             30: {20: 0.1, 25: 0.4, 30: 0.5}
         }
-        self.SHAPE_ARC = 'arc'
-        self.SHAPE_ELLIPSE = 'ellipse'
 
+    """
+        Return the next value based on the current value and its defined transitions.
+    """
     def get_next(self, current_value, transition_dict):
+        
         possibilities = list(transition_dict[current_value].keys())
         probabilities = list(transition_dict[current_value].values())
         return random.choices(possibilities, probabilities)[0]
 
+    """
+    Draws a segment of the kaleidoscope pattern based on the provided parameters.
+    """
     def draw_kaleidoscope_pattern(self, draw, cx, cy, max_radius, current_sides, current_shape, current_color_name):
-        """Draw a segment of the kaleidoscope pattern."""
         sides = self.get_next(current_sides, self.SIDES_TRANSITIONS)
         current_color = self.COLORS[current_color_name]
         
+        # Draw arcs based on the given parameters
         if current_shape == self.SHAPE_ARC:
             angle = 2 * math.pi / sides
             for i in range(sides):
@@ -73,7 +94,7 @@ class Kaleidoscope:
                 x2, y2 = cx + radius, cy + radius
                 draw.arc([x1, y1, x2, y2], math.degrees(start_angle), math.degrees(end_angle), fill=color, width=random.randint(1, 5))
                 start_angle, end_angle = start_angle + angle, end_angle + angle
-
+        # Draw ellipses based on the given parameters
         elif current_shape == self.SHAPE_ELLIPSE:
             color_name = self.get_next(current_color_name, self.COLOR_TRANSITIONS)
             color = self.COLORS[color_name]
@@ -82,34 +103,42 @@ class Kaleidoscope:
 
         return sides, current_shape, color_name
 
+    """
+        Fills the center of the image with concentric circles.
+    """
     def fill_center(self, draw, cx, cy, max_radius, current_color_name):
-        """Fill the center with concentric circles."""
         for r in range(5, int(max_radius), 1):
             color_name = self.get_next(current_color_name, self.COLOR_TRANSITIONS)
             color = self.COLORS[color_name]
             draw.ellipse((cx - r, cy - r, cx + r, cy + r), outline=color, width=1)
 
+    """
+        Creates the kaleidoscope image and saves it to the given filename.
+    """
     def create(self, filename, patterns):
-        """Create the kaleidoscope image and save to file."""
         img = Image.new('RGB', (self.WIDTH, self.HEIGHT), self.BACKGROUND_COLOR)
         draw = ImageDraw.Draw(img)
 
+        # Center of the image
         cx, cy = self.WIDTH // 2, self.HEIGHT // 2
-        current_color_name = random.choice(list(self.COLORS.keys()))
-        self.fill_center(draw, cx, cy, self.WIDTH, current_color_name)
 
+        # Initial values for color, shape, and sides
+        current_color_name = random.choice(list(self.COLORS.keys()))
         current_shape = random.choice([self.SHAPE_ARC, self.SHAPE_ELLIPSE])
         current_sides = random.choice(self.SIDES)
 
+        # Fill the center and draw patterns
+        self.fill_center(draw, cx, cy, self.WIDTH, current_color_name)
         for i in range(patterns):
             current_sides, current_shape, current_color_name = self.draw_kaleidoscope_pattern(
                 draw, cx, cy, random.uniform(10, self.WIDTH/6), current_sides, current_shape, current_color_name)
             current_shape = self.get_next(current_shape, self.SHAPE_TRANSITIONS)
             img = img.rotate(random.uniform(-5, 5))
 
+        # Save and display the image
         img.save(filename)
         img.show()
 
 if __name__ == "__main__":
     kaleidoscope = Kaleidoscope(800, 800)
-    kaleidoscope.create(filename="kaleidoscope5.png", patterns=5000)
+    kaleidoscope.create(filename="examples/kaleidoscope.png", patterns=50)
